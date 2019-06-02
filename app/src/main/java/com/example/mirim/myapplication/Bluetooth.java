1e505fcef2bfbabb8bddfd5ef5bdd3c7b9845ae2
Se-Yeong712 <lsysy712@gmail.com>
2019-06-02 23:55:09 +0900

    add Bluetooth.java

---
+++ app/src/main/java/com/example/mirim/myapplication/Bluetooth.java
@@ -0,0 +1,28 @@
+package com.example.mirim.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Bluetooth extends Service {
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
